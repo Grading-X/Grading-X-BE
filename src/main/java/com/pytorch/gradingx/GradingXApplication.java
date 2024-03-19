ef3bb2078180477a8695195274a73bcaee874172
chaejm55 <chaejm55@gmail.com>
2024-03-19 13:29:16 +0900

    [Establish] Spring Boot 환경으로 변경

---
+++ src/main/java/com/pytorch/gradingx/GradingXApplication.java
@@ -0,0 +1,13 @@
+package com.pytorch.gradingx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradingXApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GradingXApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pytorch/gradingx/GradingXApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pytorch.gradingx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradingXApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
