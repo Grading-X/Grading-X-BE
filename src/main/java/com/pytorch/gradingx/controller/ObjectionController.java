f4f5bf890b948416b134bf1b7df25d81d58876de
Gyumin Sim <gyu2781@gmail.com>
2024-03-24 15:40:04 +0900

    #5 [Comment] : Objection Controller에 API swagger 설명 작성

--- src/main/java/com/pytorch/gradingx/controller/ObjectionController.java
+++ src/main/java/com/pytorch/gradingx/controller/ObjectionController.java
@@ -13,25 +13,25 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/objection")
 public class ObjectionController {
 
-    @Operation(summary = "이의 생성", description = "이의를 생성합니다.")
+    @Operation(summary = "이의 생성", description = "이의(해당하는 문제id, 게스트email, 제목, 내용)를 요청받아 이의 생성")
     @PostMapping
     public ResponseEntity createObjection(@RequestBody ObjectionCreateRequest objectionCreateRequest){
         return ResponseEntity.ok().build();
     }
 
-    @Operation(summary = "이의 목록 조회", description = "guestEmail을 이용하여 해당 게스트의 이의 목록을 조회합니다.")
+    @Operation(summary = "이의 목록 조회", description = "guestEmail을 이용하여 해당 게스트의 이의 목록 조회(게스트)")
     @GetMapping("/guest/{guestEmail}")
     public ResponseEntity<ObjectionListResponse> findObjectionListByGuest(@PathVariable String guestEmail){
         return ResponseEntity.ok(new ObjectionListResponse());
     }
 
-    @Operation(summary = "이의 목록 조회", description = "courseId를 이용하여 해당 과목의 이의 목록을 조회합니다.")
+    @Operation(summary = "이의 목록 조회", description = "courseId에 해당하는 과목의 이의 목록 조회(교수자)")
     @GetMapping("/course/{courseId}")
     public ResponseEntity<ObjectionListResponse> findObjectionListByCourse(@PathVariable long courseId){
         return ResponseEntity.ok(new ObjectionListResponse());
     }
 
-    @Operation(summary = "이의 답변", description = "objectionId를 이용하여 해당 이의에 답변합니다.")
+    @Operation(summary = "이의 답변", description = "objectionId에 해당하는 의의 답변(제목, 내용)을 게스트 이메일로 전송")
     @PostMapping("/{objectionId}")
     public ResponseEntity updateObjection(@PathVariable long objectionId, @RequestBody ObjectionReplyRequest objectionReplyRequest){
         return ResponseEntity.ok().build();
