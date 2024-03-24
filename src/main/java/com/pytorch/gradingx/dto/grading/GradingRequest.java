8964a18e194f25a6ee2611f98dedaa44b2a1c903
chaejm55 <chaejm55@gmail.com>
2024-03-24 16:54:32 +0900

    #8 [Fix]: 채점 요청 DTO email, assignmentId로 수정

--- src/main/java/com/pytorch/gradingx/dto/grading/GradingRequest.java
+++ src/main/java/com/pytorch/gradingx/dto/grading/GradingRequest.java
@@ -12,12 +12,8 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Schema(description = "채점 요청")
 public class GradingRequest {
-    @Schema(description = "정답 ID", example = "1")
-    private Long answerId;
-    @Schema(description = "제출한 정답", example = "knu")
-    private String answer;
-    @Schema(description = "문제 ID", example = "1")
-    private Long questionId;
+    @Schema(description = "시험 ID", example = "1")
+    private Long assignmentId;
     @Schema(description = "게스트 이메일", example = "chaejm55@gmail.com")
     private String email;
 }
