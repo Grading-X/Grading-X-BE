c791f4ab4bef95619581887d9fcd928379ed24a2
Gyumin Sim <gyu2781@gmail.com>
2024-03-24 16:07:35 +0900

    #2 [Comment] : Auth관련 DTO Swagger schema 추가

--- src/main/java/com/pytorch/gradingx/dto/auth/LoginRequest.java
+++ src/main/java/com/pytorch/gradingx/dto/auth/LoginRequest.java
@@ -1,6 +1,15 @@
 package com.pytorch.gradingx.dto.auth;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+
+@AllArgsConstructor
+@Builder
+@Schema(description = "로그인 요청")
 public class LoginRequest {
+    @Schema(description = "이메일", example = "user@example.com")
     public String email;
+    @Schema(description = "비밀번호", example = "password1234")
     public String password;
 }
--- src/main/java/com/pytorch/gradingx/dto/auth/SignupRequest.java
+++ src/main/java/com/pytorch/gradingx/dto/auth/SignupRequest.java
@@ -1,10 +1,20 @@
 package com.pytorch.gradingx.dto.auth;
 
 import com.pytorch.gradingx.domain.enumeration.MemberType;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 
+@AllArgsConstructor
+@Builder
+@Schema(description = "회원가입 요청")
 public class SignupRequest {
+    @Schema(description = "이메일", example = "user@example.com")
     public String email;
+    @Schema(description = "비밀번호", example = "password1234")
     public String password;
+    @Schema(description = "이름", example = "홍길동")
     public String name;
+    @Schema(description = "멤버 타입", example = "INSTRUCTOR")
     public MemberType memberType;
 }
