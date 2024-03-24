b0a7819395bb40ce1b298d256aca052f5edfa08a
Gyumin Sim <gyu2781@gmail.com>
2024-03-24 15:37:06 +0900

    #4 [Comment] : Question Controller에 API swagger 설명 작성

--- src/main/java/com/pytorch/gradingx/controller/QuestionController.java
+++ src/main/java/com/pytorch/gradingx/controller/QuestionController.java
@@ -11,37 +11,37 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/question")
 public class QuestionController {
 
-    @Operation(summary = "문제 생성", description = "문제를 생성합니다.")
+    @Operation(summary = "문제 생성", description = "질문, 번호, 배점, 모범답안리스트, 키워드리스트를 입력받아 문제를 생성")
     @PostMapping
-    public ResponseEntity createQuestion(@RequestBody QuestionRequest questionRequest){
+    public ResponseEntity createQuestion(@RequestBody QuestionCreateRequest questionRequest){
         return ResponseEntity.ok().build();
     }
 
-    @Operation(summary = "문제 목록 조회", description = "assignmentId를 이용하여 해당 과제의 문제 목록을 조회합니다.")
+    @Operation(summary = "문제 목록 조회", description = "assignmentId에 해당하는 과제의 문제(id, 질문, 번호, 배점)리스트 반환")
     @GetMapping("/{assignmentId}")
     public ResponseEntity<QuestionListResponse> findQuestionList(@PathVariable long assignmentId){
         return ResponseEntity.ok(new QuestionListResponse());
     }
 
-    @Operation(summary = "문제 채점기준 조회", description = "questionId를 이용하여 해당 문제의 채점기준(모범답안, 키워드)리스트를 조회합니다.")
+    @Operation(summary = "문제 채점기준 조회", description = "questionId에 해당하는 문제의 채점기준(모범답안, 키워드)리스트를 반환")
     @GetMapping("/criteria/{questionId}")
     public ResponseEntity<QuestionCriteriaResponse> getQuestionCriteria(@PathVariable long questionId){
         return ResponseEntity.ok(new QuestionCriteriaResponse());
     }
 
-    @Operation(summary = "문제 상세 조회", description = "questionId를 이용하여 해당 문제의 상세 정보를 조회합니다.")
+    @Operation(summary = "문제 상세 조회", description = "guestEmail에 해당하는 guest가 응시한 문제 중 questionId에 해당하는 문제의 본인 답변과 점수 반환")
     @GetMapping("/{questionId}/{guestEmail}")
     public ResponseEntity<QuestionGuestAnswerResponse> getQuestionGuestAnswer(@PathVariable long questionId, @PathVariable String guestEmail){
         return ResponseEntity.ok(new QuestionGuestAnswerResponse());
     }
 
-    @Operation(summary = "문제 수정", description = "questionId를 이용하여 해당 문제를 수정합니다.")
-    @PutMapping("/{questionId}")
-    public ResponseEntity updateQuestion(@PathVariable long questionId, @RequestBody QuestionRequest questionRequest){
+    @Operation(summary = "문제 수정", description = "assignmentId에 해당하는 과제의 문제(questionId, 질문, 번호, 배점, 모범답변리스트, 키워드리스트)리스트를 입력받아 수정")
+    @PutMapping("/{assignmentId}")
+    public ResponseEntity updateQuestion(@PathVariable long assignmentId, @RequestBody QuestionUpdateRequest questionRequest){
         return ResponseEntity.ok().build();
     }
 
-    @Operation(summary = "문제 삭제", description = "questionId를 이용하여 해당 문제를 삭제합니다.")
+    @Operation(summary = "문제 삭제", description = "questionId에 해당하는 문제를 삭제")
     @DeleteMapping("/{questionId}")
     public ResponseEntity deleteQuestion(@PathVariable long questionId){
         return ResponseEntity.ok().build();
