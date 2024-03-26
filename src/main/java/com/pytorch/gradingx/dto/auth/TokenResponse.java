2599f08a2b886b40402579d556ecb021deceef87
Gyumin Sim <gyu2781@gmail.com>
2024-03-27 02:46:51 +0900

    #2 [Feat] : 토큰 객체를 매개변수로 하는 TokenResponse 생성자 작성

--- src/main/java/com/pytorch/gradingx/dto/auth/TokenResponse.java
+++ src/main/java/com/pytorch/gradingx/dto/auth/TokenResponse.java
@@ -1,17 +1,21 @@
 package com.pytorch.gradingx.dto.auth;
 
+import com.pytorch.gradingx.jwt.Token;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
 
-@AllArgsConstructor
 @NoArgsConstructor
-@Builder
 @Schema(description = "토큰 응답")
 public class TokenResponse {
     @Schema(description = "액세스 토큰", example = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxIiwidHlwZSI6IlVTRVIiLCJpc0FjY2VzcyI6dHJ1ZSwiaWF0IjoxNzEwMTYzNzU2LCJleHAiOjE3MTAxNjU1NTZ9.nm2x6Cspg_9jc2HiOxq05XQhyKtgM1KDfpCh5iNqt5M")
     public String accessToken;
     @Schema(description = "리프레시 토큰", example = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxIiwidHlwZSI6IlVTRVIiLCJpc0FjY2VzcyI6ZmFsc2UsImlhdCI6MTcxMDE2Mzc1NiwiZXhwIjoxNzEyNzU1NzU2fQ.1L0-OLcq-cr1zTg2Ij51AOf7OsDrwMeS_FRs0Iut9BI")
     public String refreshToken;
+
+    public TokenResponse(Token token) {
+        this.accessToken = token.getAccessToken();
+        this.refreshToken = token.getRefreshToken();
+    }
 }
