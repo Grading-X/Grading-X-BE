1f84930489c6ab3f78e18fd1cec79dfa8b8cae1c
Gyumin Sim <gyu2781@gmail.com>
2024-03-28 03:15:37 +0900

    #2 [Feat] : Jwt토큰 체크 인터셉터 구현 및 등록

---
+++ src/main/java/com/pytorch/gradingx/authentication/interceptor/TokenCheckInterceptor.java
@@ -0,0 +1,52 @@
+package com.pytorch.gradingx.authentication.interceptor;
+
+import com.pytorch.gradingx.authentication.jwt.JwtTokenGenerator;
+import com.pytorch.gradingx.authentication.jwt.JwtTokenValidator;
+import com.pytorch.gradingx.authentication.jwt.TokenType;
+import io.jsonwebtoken.JwtException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import java.security.Key;
+
+@Component
+@RequiredArgsConstructor
+public class TokenCheckInterceptor implements HandlerInterceptor {
+    private final JwtTokenGenerator jwtTokenGenerator;
+    private final JwtTokenValidator jwtTokenValidator;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        Cookie[] cookies = request.getCookies();
+        int check = 0;
+
+        if(cookies == null) {
+            throw new JwtException("Jwt토큰이 존재하지 않습니다.");
+        }
+
+        for(Cookie cookie : cookies) {
+            if(cookie.getName().equals("access-token") || cookie.getName().equals("refresh-token")) {
+                try {
+                    TokenType tokenType = cookie.getName().equals("access-token") ? TokenType.ACCESS : TokenType.REFRESH;
+                    validateToken(cookie.getValue(), tokenType);
+                    check++;
+                } catch (JwtException e) {
+                    throw new JwtException(e.getMessage());
+                }
+            }
+        }
+        if(check > 0) return true;
+
+        throw new JwtException("Jwt토큰이 존재하지 않습니다.");
+    }
+
+    private void validateToken(String token, TokenType type) throws JwtException {
+        Key secretKey = jwtTokenGenerator.createSecretKey();
+        jwtTokenValidator.validateTokenExpiration(token, secretKey);
+        jwtTokenValidator.validateTokenType(token, secretKey, type);
+    }
+}
--- src/main/java/com/pytorch/gradingx/config/WebConfig.java
+++ src/main/java/com/pytorch/gradingx/config/WebConfig.java
@@ -1,4 +1,20 @@
 package com.pytorch.gradingx.config;
 
-public class WebConfig {
+import com.pytorch.gradingx.authentication.interceptor.TokenCheckInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+    private final TokenCheckInterceptor tokenCheckInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(tokenCheckInterceptor)
+                .addPathPatterns("/api/**")
+                .excludePathPatterns("/api/auth/signup", "/api/auth/login");
+    }
 }
