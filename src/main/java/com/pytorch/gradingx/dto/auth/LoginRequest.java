713482ff501a1389789b1d9b441e23b54a031cd5
Gyumin Sim <gyu2781@gmail.com>
2024-03-28 00:47:56 +0900

    #2 [Feat] : DTO validation 추가

--- src/main/java/com/pytorch/gradingx/dto/auth/LoginRequest.java
+++ src/main/java/com/pytorch/gradingx/dto/auth/LoginRequest.java
@@ -1,15 +1,18 @@
 package com.pytorch.gradingx.dto.auth;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 
 @AllArgsConstructor
-@Builder
 @Schema(description = "로그인 요청")
 public class LoginRequest {
+    @Email
     @Schema(description = "이메일", example = "user@example.com")
     public String email;
+
+    @NotNull
     @Schema(description = "비밀번호", example = "password1234")
     public String password;
 }
--- src/main/java/com/pytorch/gradingx/dto/auth/SignupRequest.java
+++ src/main/java/com/pytorch/gradingx/dto/auth/SignupRequest.java
@@ -2,6 +2,8 @@ package com.pytorch.gradingx.dto.auth;
 
 import com.pytorch.gradingx.domain.enumeration.MemberType;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 
@@ -9,12 +11,19 @@ import lombok.Builder;
 @Builder
 @Schema(description = "회원가입 요청")
 public class SignupRequest {
+    @Email
     @Schema(description = "이메일", example = "user@example.com")
     public String email;
+
+    @NotNull
     @Schema(description = "비밀번호", example = "password1234")
     public String password;
+
+    @NotNull
     @Schema(description = "이름", example = "홍길동")
     public String name;
+
+    @NotNull
     @Schema(description = "멤버 타입", example = "INSTRUCTOR")
     public MemberType memberType;
 }
